c3ad87a8b3b42080f8aa70919d08278a3da60707
lpl <wenxiangdong@outlook.com>
2021-11-02 14:54:31 +0800

    first commit

---
+++ 130.被围绕的区域.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=130 lang=javascript
+ *
+ * [130] 被围绕的区域
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solve = function(board) {
+
+};
+// @lc code=end
+
---
+++ 62.不同路径.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=javascript
+ *
+ * [62] 不同路径
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  const dp = new Array(m).fill(new Array(n).fill(1));
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+};
+// @lc code=end
